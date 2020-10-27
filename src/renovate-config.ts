50d12a6f6e06101c3204179d3813a3374dd788b2
Jeroen de Bruijn <vv9239@gmail.com>
2020-10-27 21:17:53 +0100

    fix(repo): remove all repositories and GitHub actions update regex
    The Renovate app has added support for updating GitHub Actions in renovatebit/renovate#7434, available in v23.55.0.

--- src/renovate-config.ts
+++ src/renovate-config.ts
@@ -5,32 +5,12 @@ const branchName = 'github-renovate';
 const config: JsonObject = {
   branchPrefix: `${branchName}/`,
   dependencyDashboardTitle: 'Dependency Dashboard self-hosted',
-  enabledManagers: ['github-actions', 'regex'],
   gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
   logLevel: 'debug',
   onboarding: true,
   onboardingBranch: `${branchName}/configure`,
   platform: 'github',
-  regexManagers: [
-    {
-      datasourceTemplate: 'github-tags',
-      fileMatch: ['^\\.github/workflows/[^/]+\\.ya?ml$'],
-      matchStrings: ['uses: (?<depName>.*?)@(?<currentValue>.*?)\\s'],
-    },
-  ],
-  repositories: [
-    'vidavidorra/coap',
-    'vidavidorra/commitlint-config',
-    'vidavidorra/commitlint-plugin-function-rules',
-    'vidavidorra/docker-linux-images',
-    'vidavidorra/github-renovate',
-    'vidavidorra/global-linters',
-    'vidavidorra/homebrew-caboodle',
-    'vidavidorra/next-standard-version',
-    'vidavidorra/repo-template',
-    'vidavidorra/rigol-csv-analyser',
-    'vidavidorra/tools',
-  ],
+  repositories: [],
 };
 
 /**
